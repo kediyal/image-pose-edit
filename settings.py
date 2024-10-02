9b4b4ded952c15c9308be586e3aa0f5623b90745
amank <amankediyal@gmail.com>
2024-10-03 01:30:26 +0530

    feat: add settings.py for SAM model configurations

---
+++ settings.py
@@ -0,0 +1,7 @@
+# Keep all model configurations here
+# Update the values as per your requirements
+MODEL_CONFIG = {
+    "name": "sam_vit_h",
+    "path": "models/sam_vit_h",
+    "url": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
+}
