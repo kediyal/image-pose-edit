04eb563e87fe3945c353e7f60955ac580c1407ab
amank <amankediyal@gmail.com>
2024-10-03 01:31:43 +0530

    feat: implement model download if it doesn't already exist

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 import argparse
+import os
+
+from settings import MODEL_CONFIG
+from utils.sam_utils import download_if_model_not_exists
 
 
 # Define the function to handle command line arguments
@@ -28,8 +32,21 @@ def parse_args():
     return parser.parse_args()
 
 
+# Get the model config values
+model_name = MODEL_CONFIG["name"]
+model_path = MODEL_CONFIG["path"]
+model_url = MODEL_CONFIG["url"]
+
+# Download the model if it doesn't already exist
+download_if_model_not_exists(model_path, model_url)
+
+
 def main():
     args = parse_args()
+
+    # Ensure the path passed (via args) are valid
+    if not os.path.exists(args.image):
+        raise FileNotFoundError(f"Image file {args.image} not found.")
 
 
 if __name__ == "__main__":
