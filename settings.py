783ce19d96dd16ca7ad9e044a93f317c56c7bd4f
amank <amankediyal@gmail.com>
2024-10-03 21:44:55 +0530

    feat: add zero123 fields in settings

--- settings.py
+++ settings.py
@@ -5,4 +5,6 @@ MODEL_CONFIG = {
     "path": "models/vit_h",
     "url": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
     "yolo_model": "models/yolo11n.pt",
+    "zero123_iteration": 105000,
+    "zero123_baseurl": "https://cv.cs.columbia.edu/zero123/assets/",
 }
